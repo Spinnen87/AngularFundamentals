c55dc4a6b2f3644983dffd32a73687fcad748a93
Maxim Semikov <maxim.semikov87@gmail.com>
2023-02-12 17:43:33 +0600

    3.Create feature modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {CoursesModuleModule} from "./courses-module/courses-module.module";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {SharedModule} from "./shared/shared.module";
+import { LoginComponent } from './features/login/login.component';
+import { RegistrationComponent } from './features/registration/registration.component';
+import { CourseComponent } from './features/course/course.component';
+import {CoursesComponent} from "./features/courses/courses.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CourseComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
-    CoursesModuleModule,
     FontAwesomeModule,
     SharedModule,
   ],
