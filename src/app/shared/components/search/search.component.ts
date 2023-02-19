ed6766e8dcca0834022adbe9fa1d5cfe132f97f4
Maxim Semikov <maxim.semikov87@gmail.com>
2023-02-19 17:39:53 +0600

    task 3: 6. Create search component

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {}
+export class SearchComponent {
+  @Input() placeholder = 'Input text';
+  @Output() handelSearch = new EventEmitter<string>()
+  searchStr = '';
+
+  onSearch(){
+    this.handelSearch.emit(this.searchStr);
+  }
+
+}
 
