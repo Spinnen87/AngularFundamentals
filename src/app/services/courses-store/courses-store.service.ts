621458d4557767507a9995efbf7a3cca59592a64
Maxim Semikov <maxim.semikov87@gmail.com>
2023-03-01 23:09:39 +0600

    Router and services: 3. Implement courses API & store services

--- src/app/services/courses-store/courses-store.service.ts
+++ src/app/services/courses-store/courses-store.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {CoursesService} from "../courses/courses.service";
 import {BehaviorSubject} from "rxjs";
+import {tap} from 'rxjs/operators';
 import {Course, CreateCourse} from "../../models/courses-api-results";
 
 @Injectable({
@@ -21,49 +22,66 @@ export class CoursesStoreService {
       this.isLoading$$.next(true);
     }
 
-    this.coursesService.getAll().subscribe(data => {
-      if(data.successful){
-        this.courses$$.next(data.result);
-      }
-      this.isLoading$$.next(false);
-    });
+    this.coursesService.getAll().pipe(
+      tap(
+        data => {
+          if(data.successful){
+            this.courses$$.next(data.result);
+          }
+          this.isLoading$$.next(false);
+        }
+      )
+    );
   }
 
   createCourse(course: CreateCourse){
     this.isLoading$$.next(true);
-    this.coursesService.createCourse(course).subscribe(data => {
-      if(data.successful){
-        this.getAll();
-      }
-    });
+    this.coursesService.createCourse(course).pipe(
+      tap(data => {
+        if(data.successful){
+          this.getAll();
+        }
+      })
+    )
   }
 
   getCourse(id: string){
     this.isLoading$$.next(true);
-    this.coursesService.getCourse(id).subscribe(data => {
-      if(data.successful){
-        console.log('course data ', data.result)
-      }
-      this.isLoading$$.next(false);
-    })
+    this.coursesService.getCourse(id).pipe(
+      tap(
+        data => {
+          if(data.successful){
+            console.log('course data ', data.result)
+          }
+          this.isLoading$$.next(false);
+        })
+      )
   }
 
   editCourse(id: string, course: Course){
     this.isLoading$$.next(true);
-    this.coursesService.editCourse(id, course).subscribe(data => {
-      if(data.successful){
-        this.getAll();
-      }
-    });
+    this.coursesService.editCourse(id, course).pipe(
+      tap(
+        data => {
+          if(data.successful){
+            this.getAll();
+          }
+        }
+      )
+    )
   }
 
 
   deleteCourse(id: string){
     this.isLoading$$.next(true);
-    this.coursesService.deleteCourse(id).subscribe(data => {
-      if(data.successful){
-        this.getAll();
-      }
-    });
+    this.coursesService.deleteCourse(id).pipe(
+      tap(
+        data => {
+          if(data.successful){
+            this.getAll();
+          }
+        }
+      )
+    );
   }
 }
